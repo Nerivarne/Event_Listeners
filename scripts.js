71ec42a7290e2b7be733e76bf85955a8d5c0d52e
Nerivarne <95412701+Nerivarne@users.noreply.github.com>
2022-01-03 16:31:56 +0100

    add .preventDefault()

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
-$("button").click(function() {
+$("button").click(function(e) {
     console.log("Yeah, you clicked me");
+    e.preventDefault();
 })
 
-$("#second-button").click(function() {
+$("#second-button").click(function(g) {
     $("#first-button").text("Changed text");
+    g.preventDefault();
 })
 
-$("#third-button").click(function() {
+$("#third-button").click(function(h) {
     $("button").css("background", input);
+    h.preventDefault();
 })
 
 let input = "blue"
