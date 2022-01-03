3a280e82aed812d5a5bd0607100d7dbfe90413dc
Nerivarne <95412701+Nerivarne@users.noreply.github.com>
2022-01-03 16:30:30 +0100

    add .preventDefaul()

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
-$("button").click(function() {
+$("button").click(function(yeah) {
     console.log("Yeah, you clicked me");
+    yeah.preventDefault();
 })
 
-$("#second-button").click(function() {
+$("#second-button").click(function(e) {
     $("#first-button").text("Changed text");
+    e.preventDefault();
 })
 
-$("#third-button").click(function() {
+$("#third-button").click(function(g) {
     $("button").css("background", "pink");
+    g.preventDefault();
 })
